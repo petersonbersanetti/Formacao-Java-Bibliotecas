7f518823f4f1b8f5c48392eba5e2f3191190566f
Peterson Bersanetti <peterson.bersanetti@gmail.com>
2022-11-17 17:28:17 -0300

    String Imutavel
    String é imutavel nao pode ser mudata toLoweCase ou replace

---
+++ src/br/com/banco/bytebank/teste/TesteString.java
@@ -0,0 +1,21 @@
+package br.com.banco.bytebank.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+
+		int a =3;
+		String nome = "Alura";
+//		String outro = new String ("Alura");
+		
+//		nome.replace("A", "a");
+//		
+//		nome.toLowerCase();
+		String outra = nome.replace("A", "a");
+		System.out.println(nome);
+		System.out.println(outra);
+		
+		
+	}
+
+}
